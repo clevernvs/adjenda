42f8d2d4fc053fc82df1fbee778c44beab8c310f
Cleverton  Neves <clever.nvs@gmail.com>
2024-04-02 11:18:15 -0300

    feat: Inserindo owner User em Contact

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     class Meta:
@@ -23,9 +24,10 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
-    show =  models.BooleanField(default=True)
+    show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m', blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
