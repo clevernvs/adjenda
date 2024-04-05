f78a11bb1dc118af144564a29a703ce2dc629dbd
Cleverton  Neves <clever.nvs@gmail.com>
2024-04-05 04:18:00 -0300

    feat: Criando o template do header

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -9,6 +9,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos',
     }
 
     return render(request, 'contact/index.html', context)
@@ -16,9 +17,11 @@ def index(request):
 def contact(request, contact_id):
     # single_contact = Contact.objects.get(pk=contact_id)
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact_name = f'{single_contact.first_name} {single_contact.last_name}'
 
     context = {
         'contact': single_contact,
+        'site_title': contact_name,
     }
 
     return render(request, 'contact/contact.html', context)
